0a570fbef278c708fa376e2bbf0f2cfe199d678c
nikumaruku <74139509+nikumaruku@users.noreply.github.com>
2023-09-13 14:40:24 +0800

    Modify project details

--- src/data/data.js
+++ src/data/data.js
@@ -1,35 +1,34 @@
-import WorkIm from '../assets/projects/workImg.jpeg'
-import RealEst from '../assets/projects/realestate.jpg'
+import BookTrack from "../assets/projects/booktrack.png";
+import hotel from "../assets/projects/hotel.png";
+import recipe from "../assets/projects/recipe.png";
 
-export const data=[
-    {
-        id:1,
-        name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
-    },
-    {
-        id:2,
-        name:"React JS Application",
-        image:RealEst,
-        github:"",
-        live:"",
-    },
-    {
-        id:3,
-        name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
-    },
-    {
-        id:4,
-        name:"React JS Application",
-        image:RealEst,
-        github:"",
-        live:"",
-    },
-
-
-]
+export const data = [
+  {
+    id: 1,
+    name: "BookTrack - Book Management System",
+    image: BookTrack,
+    github: "https://github.com/nikumaruku/book-track.git",
+    live: "",
+  },
+  {
+    id: 2,
+    name: "Hotel Booking Website",
+    image: hotel,
+    github: "https://github.com/nikumaruku/Hotel-Booking-Website.git",
+    live: "",
+  },
+  {
+    id: 3,
+    name: "Recipe Notes App",
+    image: recipe,
+    github: "https://github.com/nikumaruku/web-dev-23.git",
+    live: "",
+  },
+  //   {
+  //     id: 4,
+  //     name: "Timeless Thrift",
+  //     image: RealEst,
+  //     github: "",
+  //     live: "",
+  //   },
+];
